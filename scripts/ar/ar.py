2d5f0687d8a11b00278a9ae06f0ee556d252f63c
Abhimanyu KRISHNA <abhimanyu.krishna@admins-macbook-pro-5.fritz.box>
2015-01-03 17:13:33 +0100

    Added SIFT

--- scripts/ar/ar.py
+++ scripts/ar/ar.py
@@ -5,6 +5,54 @@ import pygame, pygame.image
 from pygame.locals import *
 
 
+def set_projection_from_camera(K, width, height):
+    """ Set view from a camera calibration matrix """
+    
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    fx = K[0,0]
+    fy = K[1,1]
+    fovy = 2*arctan(0.5*height/fy)*180/pi
+    aspect = (width*fy)/(height*fx)
+    
+    # define the near and far clipping planes
+    near = 0.1
+    far = 100.0
+    
+    # set perspective
+    gluPerspective(fovy, aspect, near, far)
+    glViewport(0,0,width, height)
+
+def set_modelview_from_camera(Rt):
+    """ Set the model view matrix from camera pose. """
+    
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    
+    # Rotate teapot 90 deg around axis so that the z-axis is up
+    Rx = array([[1,0,0], [0,0,-1], [0,1,0]])
+    
+    # set rotation to best approximation
+    R = Rt[:, :3]
+    U,S,V = linalg.svd(R)
+    R = dot(U,V)
+    R[0, :] = -R[0, :] # change sign of x-axis
+    
+    # set translation
+    t = Rt[:,3]
+    
+    # setup 4*4 model view matrix
+    M = eye(4)
+    M[:3,:3] = dot(R, Rx)
+    M[:3, 3] = t
+    
+    # transpose and flatten
+    M = M.T
+    m = M.flatten()
+    
+    # replace model view with the new matrix
+    glLoadMatrixf(m)
+
 def draw_background(imname):
     """ Draw background image using a quad. """
     
